bcfa26946a08535a29573d1f531567925091474e
joe-shajan <joeshajan1551@gmail.com>
2023-09-03 15:42:43 +0530

    graphql server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,57 @@
-import { createServer } from "./server";
-// import { log } from "logger";
+import express from "express";
+import cors from "cors";
+import { graphqlHTTP } from "express-graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
-const port = process.env.PORT || 5001;
-const server = createServer();
+const app = express();
+const port = 4000;
 
-server.listen(port, () => {
-  console.log(`api running on ${port}`);
-           
+// In-memory data store
+const data = {
+  warriors: [
+    { id: "001", name: "Jaime" },
+    { id: "002", name: "Jorah" },
+  ],
+};
+
+// Schema
+const typeDefs = `
+type Warrior {
+  id: ID!
+  name: String!
+}
+
+type Query {
+  warriors: [Warrior]
+}
+`;
+
+// Resolver for warriors
+const resolvers = {
+  Query: {
+    warriors: (obj, args, context) => context.warriors,
+  },
+};
+
+const executableSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Entrypoint
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: executableSchema,
+    context: data,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Running a server at http://localhost:${port}`);
 });
