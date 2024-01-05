352b96a474341d57ccc54a9a1dbee76f90f5dd27
Binu Baiju <binub_mca2107022@ahead.students.amrita.edu>
2024-01-05 11:24:35 +0530

    signuploginagain2

--- apps/api/src/modules/bucket.ts
+++ apps/api/src/modules/bucket.ts
@@ -3,11 +3,12 @@ const AWS = require("aws-sdk");
 const ACCESSKEYID = process.env.ACCESSKEYID;
 const SECRETACCESSKEY = process.env.SECRETACCESSKEY;
 const BUCKET_NAME = process.env.BUCKET_NAME;
+const END_POINT = process.env.END_POINT;
 
 export const bucket = BUCKET_NAME;
 
 export const s3 = new AWS.S3({
-  endpoint: "http://s3-ap-south-1.amazonaws.com",
+  endpoint: END_POINT,
   accessKeyId: ACCESSKEYID,
   secretAccessKey: SECRETACCESSKEY,
   sslEnabled: false,
