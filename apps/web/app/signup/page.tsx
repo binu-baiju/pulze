6ed5a3315868214357ca21b531589fb21823b2ab
anshultiwari95 <anshultiwari95@gmail.com>
2024-02-24 16:17:16 +0530

    Updated Signup Page

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "ui/lib/utils";
+import SignUpPAgeImage from "../../icons/SignUpPageImage";
 
 //import { buttonVariants } from "@/registry/new-york/ui/button"
 import { UserAuthForm } from "./components/user-auth-form";
@@ -30,93 +31,41 @@ export const metadata: Metadata = {
 
 const App: React.FunctionComponent = () => {
   return (
-    <>
-      <div className="md:hidden">
-        {/* <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        /> */}
-      </div>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/login"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Pulze
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
-        </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+    <div className="h-screen flex items-center justify-center">
+      <div className="h-full w-[45%] flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center  w-[400px] h-[450px] mt-[80px]">
+          <h1 className="font-semibold font-[Inter] sm:text-[33px] text-center">
+            Visually Rich sync conversations - free forever
+          </h1>
+          <div className=" w-[400px] h-[450px] mt-[100px] items-center">
+            <UserAuthForm />
+            <button className="font-exbold bg-[#8645FF] h-10 text-[#F3E8FF] rounded-lg text-xl w-full mb-2">
+              Continue with email
+            </button>
+            <div className="ml-[0px]">
+              <p className=" text-center text-xs text-muted-foreground font-poppins font-light">
+                Have an account?
+                <Link
+                  href="../login"
+                  className="text-[#8B5CF6] font-bold font-[Inter] text-xs"
+                >
+                  Login
+                </Link>{" "}
               </p>
             </div>
-            <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
           </div>
         </div>
       </div>
-    </>
+      <div className="h-full w-[55%] relative">
+        <SignUpPAgeImage className="max-w-full max-h-full mr-[100px]" />
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white mt-[20px] w-[540px] h-[450px]">
+          <p className="font-semibold font-[Inter] text-[48px] tracking-wide ">
+            "Amazing tool to help collaborate with the team async. Very valuable
+            especially in the remote-first times today!"
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
