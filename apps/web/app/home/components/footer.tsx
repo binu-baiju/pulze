ac7cad297ea02d1249bd0f31842c642184c4a7e5
anshultiwari95 <anshultiwari95@gmail.com>
2024-03-20 01:10:17 +0530

    Updated HomePage Components

--- apps/web/app/home/components/footer.tsx
+++ apps/web/app/home/components/footer.tsx
@@ -6,26 +6,37 @@ import LinkedInIcon from "../../../icons/LinkedInIcon";
 
 const Footer = () => {
   return (
-    <div className="footer pt-60 border-2 overflow-auto bg-gradient-to-t from-purple-500 via-purple-100 to-white">
-      <div className="containe text-center">
-        <div className="flex flex-col justify-center items-start ml-64">
-          <p className="font-poppins font-bold text-2xl">pulze</p>
-          <p className="font-poppins font-bold text-xl flex flex-row">
-            Built by an async team around the world
-            <GlobeIcon className="scale-[45%] mt-[-15px] ml-[-10px]" />
-          </p>
-          <p className="font-poppins font-light text-xl flex flex-row">
-            Get in touch
-            <span className="flex flex-row">
-              <EnvelopeIcon />
-              <TwitterIcon />
-              <LinkedInIcon />
-            </span>
-            <span className="font-poppins font-light text-xl ml-[420px]">
-              All Rights <span className="font-bold">&copy;</span> pulze
-            </span>
+    <div className="py-40 bg-gradient-to-t from-purple-500 via-purple-100 to-white">
+      <div className="flex flex-row justify-evenly flex-wrap">
+        <div>
+          <div>
+            <p className="font-poppins font-bold text-2xl">Pulze</p>
+          </div>
+          <div className="flex flex-row justify-center content-center">
+            <p className="font-poppins font-bold text-xl mt-4">
+              Built by an Anshul Tiwari And Binu
+            </p>
+            <div>
+              <GlobeIcon className="scale-[45%]" />
+            </div>
+          </div>
+          <div>
+            <p className="font-poppins font-light text-xl flex flex-row">
+              Get in touch
+              <span className="flex flex-row">
+                <EnvelopeIcon />
+                <TwitterIcon />
+                <LinkedInIcon />
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="flex content-end mt-20">
+          <p className="font-poppins font-light text-xl">
+            All Rights <span className="font-bold">&copy;</span> pulze
           </p>
         </div>
+        <div></div>
       </div>
     </div>
   );
